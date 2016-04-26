1ad26e4ab225f4a1596537c6d7e6c802346df3e4
elmhaidara <elmhaidara@gmail.com>
2016-04-26 14:45:30 +0200

    utilisation de l'héritage pour les commentaires

--- apps/commentaires/models.py
+++ apps/commentaires/models.py
@@ -1,41 +1,23 @@
 from django.db import models
 
 
-
 # Create your models here.
+class Commentaire(models.Model):
+    texte = models.TextField(blank=False, null=False)
+    datetime = models.DateTimeField(auto_now_add=True)
+    utilisateur = models.ForeignKey('gensdujardin.Utilisateur', on_delete=models.CASCADE)
 
-class CommentairePlante(models.Model):
-  texte = models.TextField(blank = False, null = False)
-  datetime = models.DateTimeField( auto_now_add = True )
-  utilisateur = models.ForeignKey(
-    'gensdujardin.Utilisateur',
-    on_delete = models.CASCADE
-  )
-  plante = models.ForeignKey(
-    'jardin.Plante',
-    on_delete = models.CASCADE
-  )
+    class Meta:
+        abstract = True
 
-class CommentaireJardin(models.Model):
-  texte = models.TextField(blank = False, null = False)
-  datetime = models.DateTimeField( auto_now_add = True )
-  utilisateur = models.ForeignKey(
-    'gensdujardin.Utilisateur',
-    on_delete = models.CASCADE
-  )
-  jardin = models.ForeignKey(
-    'jardin.Jardin',
-    on_delete = models.CASCADE
-  )
 
-class CommentaireLopin(models.Model):
-  texte = models.TextField(blank = False, null = False)
-  datetime = models.DateTimeField( auto_now_add = True )
-  utilisateur = models.ForeignKey(
-    'gensdujardin.Utilisateur',
-    on_delete = models.CASCADE
-  )
-  lopin = models.ForeignKey(
-    'jardin.Lopin',
-    on_delete = models.CASCADE
-  )
+class CommentairePlante(Commentaire):
+    plante = models.ForeignKey('jardin.Plante', on_delete=models.CASCADE)
+
+
+class CommentaireJardin(Commentaire):
+    jardin = models.ForeignKey('jardin.Jardin', on_delete=models.CASCADE)
+
+
+class CommentaireLopin(Commentaire):
+    lopin = models.ForeignKey('jardin.Lopin', on_delete=models.CASCADE)
