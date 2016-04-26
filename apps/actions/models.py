b05804004be5fea9e531477271ba3e4ca7de7ad2
elmhaidara <elmhaidara@gmail.com>
2016-04-26 13:09:49 +0200

    ajout de l'application actions + maj interface admin

---
+++ apps/actions/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+# Create your models here.
+class TypeAction(models.Model):
+    nom = models.CharField(max_length=100, help_text="Nom de l'action")
+
+    def __str__(self):
+        return "{}".format(self.nom)
+
+
+class Action(models.Model):
+    type_action = models.ForeignKey(TypeAction)
+    utilisateur = models.ForeignKey('gensdujardin.Utilisateur', on_delete=models.CASCADE)
+    lopin = models.ForeignKey('jardin.Lopin', on_delete=models.CASCADE)
+    plante = models.ForeignKey('jardin.Plante', on_delete=models.CASCADE)
+    date_creation = models.DateTimeField(auto_now_add=True, help_text="Date de création de l'action",
+                                         verbose_name="Date de création")
+
+    def __str__(self):
+        if self.lopin is None:
+            return "{} le {} par {} sur {}".format(self.type_action.nom, self.date_creation,
+                                                   self.utilisateur.user.username, self.plante)
+        else:
+            return "{} le {} par {} sur {}".format(self.type_action.nom, self.date_creation,
+                                                   self.utilisateur.user.username, self.lopin)
