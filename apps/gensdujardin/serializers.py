22d7dc7951c6eeb7638577be27b75e58be49365b
hverlin <hugues.verlin@gmail.com>
2016-04-28 11:12:45 +0200

    modif settings

--- apps/gensdujardin/serializers.py
+++ apps/gensdujardin/serializers.py
@@ -1,19 +1,27 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from apps.gensdujardin.models import Profil
 
 
 class ProfilSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Profil
         fields = ('id', 'ville', 'presentation', 'avatar', 'user')
+
 
 class UserSerializer(serializers.ModelSerializer):
     profil = ProfilSerializer(many=False)
 
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ('id', 'username', 'password', 'first_name', 'last_name', 'email', 'profil')
 
+    def create(self, validated_data):
+        user = get_user_model().objects.create(
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
 
+        return user
--- pldjardin/settings.py
+++ pldjardin/settings.py
@@ -130,9 +130,6 @@ LOCALE_PATHS = [
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
