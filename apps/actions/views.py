524d96a7b07aee7706d251ee869df6d314dd410b
Loïc Touzard <loic.touzard@gmail.com>
2016-05-02 09:48:54 +0200

    Add route /actions/types with http GET, to retreive the avalables action for a use

--- apps/actions/views.py
+++ apps/actions/views.py
@@ -1,4 +1,7 @@
-from rest_framework import viewsets, mixins
+from django.http import HttpResponse
+from rest_framework import viewsets, mixins, status
+from rest_framework.decorators import list_route
+from rest_framework.renderers import JSONRenderer
 
 from apps.actions.models import Action
 from apps.actions.serializers import ActionFullSerializer, ActionCreateSerializer
@@ -23,4 +26,12 @@ class ActionViewSet(mixins.CreateModelMixin,
         else:
             return ActionFullSerializer
 
+    @list_route(methods=["GET"])
+    def types(self, request):
+        # retourner les paire clé/valeur des types d'action disponible a la création pour un utilisateur
+        res = {"types":[]}
+        for type in Action.ALL_ACTION:
+            if type[0] in Action.USER_AVAILABLE_ACTION:
+                res["types"].append(type)
+        return HttpResponse(JSONRenderer().render(res), content_type="application/json", status=status.HTTP_200_OK)
 
